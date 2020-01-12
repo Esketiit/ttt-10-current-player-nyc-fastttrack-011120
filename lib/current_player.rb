e5abccbf933b3ba3b9888487eb90f2fa295fbc68
Brian Desplantes <heftygac@gmail.com>
2020-01-12 17:05:32 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
-def #turn_count (board)
+def turn_count (board)
   turns = 0;
 
   board.each do |spot|
     if (spot == "X" || spot == "O")
-      turn++
+      turn += 1
     end
   end
   return turns
